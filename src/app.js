19ee47da4d5c2cc1bbf5edb4d4b199ce5ecffa6d
Lee <57182164+LeeJay124@users.noreply.github.com>
2022-11-23 14:28:35 -0500

    set cors back

--- src/app.js
+++ src/app.js
@@ -7,19 +7,19 @@ const ordersRouter = require("./orders/orders.router");
 const dishesRouter = require("./dishes/dishes.router");
 
 const app = express();
-const allowlist = ['https://starter-grub-dash-front-end-seven.vercel.app',]
-const corsOptionsDelegate = function (req, callback) {
-  const corsOptions={};
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// const allowlist = ['https://starter-grub-dash-front-end-seven.vercel.app',]
+// const corsOptionsDelegate = function (req, callback) {
+//   const corsOptions={};
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 // You have not learned about CORS yet.
 // The following line let's this API be used by any website.
-app.use(cors(corsOptionsDelegate));
+app.use(cors());
 app.use(express.json());
 
 app.use("/dishes", dishesRouter);
